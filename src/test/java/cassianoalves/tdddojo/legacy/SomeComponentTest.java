862fca8a128af3ee34e62d3e2c775ff7816ca311
Cassiano Alves <calves@bionexo.com>
2019-10-28 19:21:37 -0300

    Adding legacy code example with PowerMock

--- src/test/java/cassianoalves/tdddojo/legacy/SomeComponentTest.java
+++ src/test/java/cassianoalves/tdddojo/legacy/SomeComponentTest.java
@@ -1,14 +1,35 @@
 package cassianoalves.tdddojo.legacy;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(FactorDaoFactory.class)
 public class SomeComponentTest {
+
+    @Mock
+    private FactorDao factorDao;
+
+    @InjectMocks
+    private SomeComponent someComponent;
 
     @Test
     public void testMultiplyByFactorOfCode() {
-        
+        PowerMockito.mockStatic(FactorDaoFactory.class);
+
+        Mockito.when(FactorDaoFactory.getInstance()).thenReturn(factorDao);
+        Mockito.when(factorDao.getFactor(123)).thenReturn(10);
+        int result = someComponent.multiplyByFactorOfCode(123, 20);
+
+        assertEquals(200, result);
     }
 
 }
